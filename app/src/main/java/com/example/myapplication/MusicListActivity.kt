2cfcceaa090440a44cae54aee32b304f63c93762
overfloatGame <3364295743@qq.com>
2023-11-25 18:16:11 +0800

    musicPlayer

--- app/src/main/java/com/example/myapplication/MusicListActivity.kt
+++ app/src/main/java/com/example/myapplication/MusicListActivity.kt
@@ -1,24 +1,34 @@
 package com.example.myapplication
 
 import MusicDatabase
+import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-
 class MusicListActivity : ComponentActivity() {
     private lateinit var musicListView:RecyclerView
+    private lateinit var back1:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.music_list_activity)
         musicListView=findViewById(R.id.MusicListView)
+        back1=findViewById((R.id.back1))
+
+        back1.setOnClickListener{
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
 
         val musicList=getMusicList()
 
@@ -36,7 +46,7 @@ class MusicListActivity : ComponentActivity() {
 
         val cursor=db.query(
             MusicDatabase.TABLE_NAME,
-            null,
+            arrayOf(MusicDatabase.COLUMN_NAME,MusicDatabase.COLUMN_ID,MusicDatabase.COLUMN_ARTIST,MusicDatabase.COLUMN_DURATION,MusicDatabase.COLUMN_DATA,MusicDatabase.COLUMN_MIMETYPE,MusicDatabase.COLUMN_SIZE,MusicDatabase.COLUMN_ALBUM_ID),
             null,
             null,
             null,
@@ -52,9 +62,10 @@ class MusicListActivity : ComponentActivity() {
             val data = cursor.getString(cursor.getColumnIndexOrThrow(MusicDatabase.COLUMN_DATA))
             val mimeType = cursor.getString(cursor.getColumnIndexOrThrow(MusicDatabase.COLUMN_MIMETYPE))
             val size = cursor.getLong(cursor.getColumnIndexOrThrow(MusicDatabase.COLUMN_SIZE))
+            val albumId=cursor.getLong(cursor.getColumnIndexOrThrow(MusicDatabase.COLUMN_ALBUM_ID))
 
             if (name != null && artist != null && data != null && mimeType != null) {
-                val music = Music(name, artist, id, data, duration, size, mimeType)
+                val music = Music(name, artist, id, data, duration, size, mimeType,albumId)
                 musicList.add(music)
             }
         }
@@ -66,13 +77,57 @@ class MusicListActivity : ComponentActivity() {
 
 data class Music(
     val name: String, val artist: String, val id: Int, val data: String, val duration: Int, val size: Long, val mimeType: String,
-    var isCollected:Boolean=false)
+    val albumId:Long,var isCollected:Boolean=true)
 
-class MusicListAdapter(private val musicList: List<Music>,private val context: Context):RecyclerView.Adapter<MusicListAdapter.ViewHolder>(){
+class MusicListAdapter(private val musicList: List<Music>, context: Context):RecyclerView.Adapter<MusicListAdapter.ViewHolder>(){
+
+    private val dbHelper=MusicDatabase(context)
+    val db=dbHelper.writableDatabase
+    private val sharedPreferences=context.getSharedPreferences("isCollected",Context.MODE_PRIVATE)
 
     inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
         val musicName: TextView =view.findViewById(R.id.musicName)
         val artistName1: TextView =view.findViewById(R.id.artistName1)
+        val picture1:ImageView=view.findViewById(R.id.picture1)
+
+        val likeButton1: Button =itemView.findViewById(R.id.MyLike_button1)
+        init {
+            likeButton1.setOnClickListener{
+                val position = bindingAdapterPosition
+                if(position!=RecyclerView.NO_POSITION) {
+                    val music = musicList[position]
+                    val isCollected=sharedPreferences.getBoolean("isCollected_${music.id}",false)
+                    if (!isCollected) {
+                        likeButton1.setBackgroundResource(R.drawable.heart)
+                        val editor=sharedPreferences.edit()
+                        editor.putBoolean("isCollected_${music.id}",true)
+                        editor.apply()
+
+                        val values= ContentValues().apply{
+                            put(MusicDatabase.COLUMN_NAME,music.name)
+                            put(MusicDatabase.COLUMN_ARTIST,music.artist)
+                            put(MusicDatabase.COLUMN_ID,music.id)
+                            put(MusicDatabase.COLUMN_DURATION,music.duration)
+                            put(MusicDatabase.COLUMN_MIMETYPE,music.mimeType)
+                            put(MusicDatabase.COLUMN_SIZE,music.size)
+                            put(MusicDatabase.COLUMN_DATA,music.data)
+                            put(MusicDatabase.COLUMN_ALBUM_ID,music.albumId)
+                        }
+                        db.insert(MusicDatabase.TABLE_NAME,null,values)
+                    } else {
+                        likeButton1.setBackgroundResource(R.drawable.isheart)
+                        val editor=sharedPreferences.edit()
+                        editor.putBoolean("isCollected_${music.id}",false)
+                        editor.apply()
+
+                        val selection = "${MusicDatabase.COLUMN_NAME}=? AND ${MusicDatabase.COLUMN_ARTIST}=? AND ${MusicDatabase.COLUMN_ID}=? AND ${MusicDatabase.COLUMN_DURATION}=? AND ${MusicDatabase.COLUMN_MIMETYPE}=? AND ${MusicDatabase.COLUMN_SIZE}=? AND ${MusicDatabase.COLUMN_DATA}=? AND ${MusicDatabase.COLUMN_ALBUM_ID}=?"
+                        val selectionArgs= arrayOf(music.name, music.artist, music.id.toString(), music.duration.toString(), music.mimeType, music.size.toString(), music.data,music.albumId.toString())
+                        db.delete(MusicDatabase.TABLE_NAME,selection,selectionArgs)
+                    }
+                    music.isCollected = !music.isCollected
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -84,6 +139,13 @@ class MusicListAdapter(private val musicList: List<Music>,private val context: C
         val music=musicList[position]
         holder.musicName.text=music.name
         holder.artistName1.text=music.artist
+
+        val isCollected=sharedPreferences.getBoolean("isCollected_${music.id}",false)
+        if(isCollected){
+            holder.likeButton1.setBackgroundResource(R.drawable.heart)
+        }else{
+            holder.likeButton1.setBackgroundResource(R.drawable.isheart)
+        }
     }
 
     override fun getItemCount()=musicList.size
--- app/src/main/java/com/example/myapplication/MusicPlayerFragment.kt
+++ app/src/main/java/com/example/myapplication/MusicPlayerFragment.kt
@@ -4,4 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
+class MusicPlayerFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val myFragment = MusicPlayerFragment()
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.FrameLayout_main, myFragment).commit()
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.FrameLayout_music,myFragment).commit()
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.FrameLayout_song,myFragment).commit()
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_music_player, container, false)
+    }
+}
