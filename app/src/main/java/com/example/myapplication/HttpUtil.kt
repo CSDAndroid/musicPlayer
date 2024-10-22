25ecd336fd2253e6c1b709634f90491bf071043b
overfloatGame <3364295743@qq.com>
2024-10-22 10:16:48 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/build.gradle.kts
    #	app/src/main/java/com/example/myapplication/MusicListActivity.kt
    #	app/src/main/java/com/example/myapplication/MusicPlayerFragment.kt
    #	app/src/main/java/com/example/myapplication/SongListActivity.kt
    #	app/src/main/res/layout/fragment_music_player.xml

---
+++ app/src/main/java/com/example/myapplication/HttpUtil.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+//定义一个单例类,用于重复使用
+object HttpUtil {
+    //这是一个泛型函数，接受一个地址 address 和一个服务类 serviceClass，并返回一个泛型类型 T 的结果。
+    fun <T : Any> sendHttp(address: String, serviceClass: Class<T>): T {
+
+        //创建客户端实例,用于配置和定制 HTTP 请求的行为，例如超时时间、拦截器等。
+        val client = OkHttpClient.Builder()
+            .connectTimeout(300L, TimeUnit.MILLISECONDS)
+            .writeTimeout(1, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+
+        //创建retrofit实例,可以用来创建 API 接口的实例，发起网络请求等操作
+        val retrofit = Retrofit.Builder()
+            .baseUrl(address)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())//json转换器
+            .build()
+
+        //使用 Retrofit 实例创建一个特定类型的服务接口实例，该接口由参数 serviceClass 指定，并将其返回。
+        return retrofit.create(serviceClass)
+    }
+}
--- app/src/main/java/com/example/myapplication/HttpsMusic.kt
+++ app/src/main/java/com/example/myapplication/HttpsMusic.kt
@@ -15,14 +15,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
 import org.json.JSONObject
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import java.util.concurrent.TimeUnit
 
 class HttpsMusic : ComponentActivity() {
     private lateinit var httpListView: RecyclerView
@@ -180,29 +176,5 @@ class HttpsMusic : ComponentActivity() {
 
         @GET("/search")
         suspend fun getMusicKey(@Query("keywords") keywords: String): Response<JsonObject>
-    }
-}
-
-//定义一个单例类,用于重复使用
-object HttpUtil {
-    //这是一个泛型函数，接受一个地址 address 和一个服务类 serviceClass，并返回一个泛型类型 T 的结果。
-    fun <T : Any> sendHttp(address: String, serviceClass: Class<T>): T {
-
-        //创建客户端实例,用于配置和定制 HTTP 请求的行为，例如超时时间、拦截器等。
-        val client = OkHttpClient.Builder()
-            .connectTimeout(300L, TimeUnit.MILLISECONDS)
-            .writeTimeout(1, TimeUnit.SECONDS)
-            .retryOnConnectionFailure(true)
-            .build()
-
-        //创建retrofit实例,可以用来创建 API 接口的实例，发起网络请求等操作
-        val retrofit = Retrofit.Builder()
-            .baseUrl(address)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())//json转换器
-            .build()
-
-        //使用 Retrofit 实例创建一个特定类型的服务接口实例，该接口由参数 serviceClass 指定，并将其返回。
-        return retrofit.create(serviceClass)
     }
 }
